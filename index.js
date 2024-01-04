6b50c6c6d4083f35b21c10e4713a299b067f46b3
Geremy Ibobi <geremy.ibobi@yahoo.fr>
2024-01-04 18:43:29 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+http.createServer(function(req, res) {
+
+res.write('On the way to being a full stack developer');
+res.end();
+}
+).listen(3000)
+
+console.log('Server is listening on port 3000');
